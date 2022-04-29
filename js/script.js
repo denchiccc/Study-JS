82c7c378790dafb15bdef21748996bd9e416d484
Denis <denurchenko1981@gmail.com>
2022-04-29 09:55:47 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict";
+/* let namber = 5;
+const leftBordWidth = 1;
+namber = 10;
+console.log(namber);
+
+const obj = {
+    userName: "Jhon",
+    userOld: 25,
+    isMarreid: false
+};
+console.log(obj.userOld);
+
+const arr = [1, 2, 3];
+console.log(arr[1])
+
+const obt = {a: 1, b: 2, d: 3};
+ */
+// alert('Hello')
+/* const result = confirm('Are you ok?');
+console.log(result); */
+/* const answer = prompt('are you 18 old', '18');
+console.log(typeof(answer)); */
+ const answers = [];
+ answers[0] = prompt('What is your name?', '');
+ answers[1] = prompt('What is your last name?', '');
+ answers[2] = prompt('How old are you?', '');
+ answers[3] = prompt('Are you from?', '');
+ answers[4] = prompt('How are you?', '');
+
+ document.write(answers);
+
+
